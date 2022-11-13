994c03a37317d483c17fb310d00d4f9e15269705
ThienBui1801 <buiquythien1801@gmail.com>
2022-11-13 20:19:44 +0700

    push

--- src/main.js
+++ src/main.js
@@ -5,10 +5,19 @@ const morgan = require("morgan");
 const app = express();
 const port = 3000;
 
+const route = require('./routes')
+
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// HMLHttpRequest, fetch, axios
 
 // HTTP logger
 app.use(morgan("combined"));
+
+// Action ---> Dispatcher ---> Function handler
 
 // Handlebars
 app.engine(
@@ -20,13 +29,10 @@ app.engine(
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
-app.get("/", function (req, res) {
-  res.render("home");
-});
+// Home, Search, Contact
 
-app.get("/news", function (req, res) {
-  res.render("New");
-});
+// Router Init
+route(app);
 
 app.listen(port, function () {
   console.log("This is my Nodejs");
